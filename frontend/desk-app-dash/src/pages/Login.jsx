1e36309f90a55883297d5693159d808456c1e4ad
Lucas LBF Furango <lucas.software.engineering@gmail.com>
2026-05-16 19:23:20 -0300

    login screen where users log in using their username and password

--- frontend/desk-app-dash/src/pages/Login.jsx
+++ frontend/desk-app-dash/src/pages/Login.jsx
@@ -1,8 +1,45 @@
+import "../styles/form.css";
+
 function Login() {
     return (
         <div>
             <div className="bar-top">
                 <h1>RE-PRAY</h1>
+            </div>
+            <div className="camp">
+                <div className="camp-form">
+                    <div>
+                        <h2>Tela de Login</h2>
+                        <hr />
+                    </div>
+                    <div>
+                        <label for="pedido" class="block text-sm font-medium text-slate-300 mb-2">
+                            Usuario
+                        </label>
+                        <input 
+                            id="pedido" 
+                            name="pedido" 
+                            rows="5" 
+                            required
+                        ></input>
+                    </div>
+                    <div>
+                        <label for="pedido" class="block text-sm font-medium text-slate-300 mb-2">
+                            Senha
+                        </label>
+                        <input 
+                            id="pedido" 
+                            name="pedido" 
+                            rows="5" 
+                            required
+                        ></input>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "end" }}>
+                        <button type="submit" className="on">
+                            Entrar no Sistema
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
